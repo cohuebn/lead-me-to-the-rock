3bd34b6d43a3cf170788122ed83f4b627c0af962
Cory Huebner <chuebner86@gmail.com>
2017-12-24 14:20:36 -0600

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { routes } from './routes';
 import { HeaderComponent } from './header/header.component';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { AboutComponent } from './about/about.component';
+import { FaithComponent } from './faith/faith.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, AboutComponent,
+    FaithComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/faith/faith.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'faith',
+    templateUrl: './faith.component.html',
+    styleUrls: ['./faith.component.scss']
+})
+export class FaithComponent {
+    
+}
